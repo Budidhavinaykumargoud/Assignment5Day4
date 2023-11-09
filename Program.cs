659d8280d03e1d63a81fda822a7bae63f65c864e
Budidhavinaykumargoud <vinaygoud911@gmail.com>
2023-11-09 14:50:19 +0530

    AssignVarDataType

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignVarDataType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            {
+                // Declare and initialize variables
+                string name = "Vinay";
+                int age = 23;  // Just a symbolic age for fun
+                double height = 174.0;  // Just a symbolic height for fun
+
+                // Display variables
+                Console.WriteLine("Name: " + name);
+                Console.WriteLine("Age: " + age);
+                Console.WriteLine("Height: " + height + " cm");
+                Console.ReadLine();
+            }
+        }
+
+    }
+}
